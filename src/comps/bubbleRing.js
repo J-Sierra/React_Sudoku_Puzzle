16b3a3bb59e261bf02ed91263e82dba11961dbf4
Johnny Sierra <Gjohnnys@att.net>
2023-02-26 16:38:38 -0600

    updates to connect in cell.js

--- src/comps/bubbleRing.js
+++ src/comps/bubbleRing.js
@@ -1,28 +1,25 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import NumberBubble from "./numberBubble";
-import './styles/bubbleRing.scss'
+import "./styles/bubbleRing.scss";
 
 class BubbleRing extends Component {
+  render() {
+    let { cellChange } = this.props;
+    return (
+      <div id="BubbleRing">
+        {Array.from(Array(9).keys()).map((e, i) => (
+          <NumberBubble
+            cell={this.props.cell}
+            cellChange={cellChange}
+            number={i + 1}
+            key={i}
+          />
+        ))}
 
-
-    render() {
-        let {cellChange} = this.props
-        return (
-            <div id='BubbleRing'>
-
-                {Array.from(Array(9).keys()).map((e, i) =>
-                    <NumberBubble
-                        cellChange={cellChange}
-                        number={i + 1}
-                        key ={i*Math.random()}/>
-                )}
-
-                <NumberBubble
-                    props={this.props}
-                    number={'X'}/>
-            </div>
-        )
-    }
+        <NumberBubble props={this.props} number={"X"} />
+      </div>
+    );
+  }
 }
 
 export default BubbleRing;
--- src/comps/cell.js
+++ src/comps/cell.js
@@ -1,126 +1,99 @@
-import React, {Component}             from 'react';
-import "./styles/cell.scss"
-import {onCellChange, onCellSelected} from "./redux/actions/actions";
-import {connect}                      from "react-redux";
-import BubbleRing                     from "./bubbleRing";
+import React, { Component } from "react";
+import "./styles/cell.scss";
+import { onCellChange, onCellSelected } from "./redux/actions/actions";
+import { connect } from "react-redux";
+import BubbleRing from "./bubbleRing";
 
 class Cell extends Component {
-    state = {
-        editable: true,
-        editing : false,
-        selected: false
+  state = {
+    editable: true,
+    editing: false,
+    selected: false,
+  };
+
+  componentDidMount() {
+    if (this.props.cell.number) {
+      this.setState({
+        ...this.state,
+        editable: false,
+      });
     }
-    
-    componentDidMount() {
-        if(this.props.cell.number) {
-            this.setState({
-                              ...this.state,
-                              editable: false
-                          })
-        }
-    }
-    
-    render() {
-        let {cell} = this.props
-        let {selected} = cell
-        let {
-                editing,
-                editable
-            } = this.state
-        let cellData = cell.number
-        
-        return (
-            
-            <div className={editable
-                ? "cell"
-                : "boldCell"}
-                //style={cellSelected ? {backgroundColor: "lightblue"} : null}
-                 style={selected
-                     ? {backgroundColor: "gray"}
-                     : null}
-                 onClick={this.selectCell}
-                 onBlur={this.toggle}>
-                {!editing
-                    ? cellData
-                        ? cellData
-                        : null
-                    :
-                    <>
-                        <input
-                            id={"cellInput"}
-                            type="text"
-                            autoComplete={"off"}
-                            autoFocus={true}
-                            maxLength={1}
-                            onChange={this.handleOnChange}
-                            placeholder={cellData}
-                        />
-                        <BubbleRing
-                            cellChange={this.handleOnChange}
-                            cell={cell}/>
-                    </>
-                    
-                }
-            
-            </div>
-        );
-    }
-    
-    selectCell = () => {
-        let {
-                cellSelected,
-                cell
-            } = this.props
-        this.toggle()
-        cellSelected(cell)
-        
-    }
-    
-    toggle = () => {
-        let {
-                editable,
-                editing
-            } = this.state
-        
-        if(editable) this.setState({editing: !editing})
-        
-    }
-    
-    handleOnChange = (e) => {
-        let {
-                cellOnChange,
-                cell
-            } = this.props
-        let {value} = e.target
-        
-        !isNaN(value) && value !== '0'
-            ? cellOnChange(value, cell)
-            : cellOnChange(null, cell)
-        
-        this.toggle()
-    }
-    
+  }
+
+  render() {
+    let { cell } = this.props;
+    let { selected } = cell;
+    let { editing, editable } = this.state;
+    let cellData = cell.number;
+
+    return (
+      <div
+        className={editable ? "cell" : "boldCell"}
+        //style={cellSelected ? {backgroundColor: "lightblue"} : null}
+        style={selected ? { backgroundColor: "gray" } : null}
+        onClick={this.selectCell}
+        onBlur={this.toggle}
+      >
+        {!editing ? (
+          cellData ? (
+            cellData
+          ) : null
+        ) : (
+          <>
+            <input
+              id={"cellInput"}
+              type="text"
+              autoComplete={"off"}
+              autoFocus={true}
+              maxLength={1}
+              onChange={this.handleOnChange}
+              placeholder={cellData}
+            />
+            <BubbleRing cell={cell} cellChange={this.handleOnChange} />
+          </>
+        )}
+      </div>
+    );
+  }
+
+  selectCell = () => {
+    let { cellSelected, cell } = this.props;
+    this.toggle();
+    cellSelected(cell);
+  };
+
+  toggle = () => {
+    let { editable, editing } = this.state;
+
+    if (editable) this.setState({ editing: !editing });
+  };
+
+  handleOnChange = (e) => {
+    let { cellOnChange, cell } = this.props;
+    let { value } = e.target;
+
+    !isNaN(value) && value !== "0"
+      ? cellOnChange(value, cell)
+      : cellOnChange(null, cell);
+
+    this.toggle();
+  };
 }
 
-const mapStateToProps = (state, ownProps) => {
-    const {
-              gameBoard,
-              sectors,
-              gameBoardReady
-          } = state.gameBoardReducer
-    return {
-        gameBoard,
-        sectors,
-        gameBoardReady,
-        
-    }
-}
+const mapStateToProps = (state) => {
+  const { gameBoard, sectors, gameBoardReady } = state.gameBoardReducer;
+  return {
+    gameBoard,
+    sectors,
+    gameBoardReady,
+  };
+};
 
 function mapActionsToProps() {
-    return {
-        cellOnChange: onCellChange,
-        cellSelected: onCellSelected
-    }
+  return {
+    cellOnChange: onCellChange,
+    cellSelected: onCellSelected,
+  };
 }
 
-export default connect(mapStateToProps, mapActionsToProps())(Cell)
+export default connect(mapStateToProps, mapActionsToProps)(Cell);
