8c1f91b9342dd62460a67b5e90017856b77798c4
Johnny Sierra <Gjohnnys@att.net>
2023-02-26 19:32:41 -0600

    updates to connect in cell.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './comps/styles/index.scss';
-import App from './app';
-import * as serviceWorker from './serviceWorker';
-import {Provider} from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./comps/styles/index.scss";
+import { Provider } from "react-redux";
+import App from "./app";
+import * as serviceWorker from "./serviceWorker";
 import store from "./comps/redux/store";
 
-ReactDOM.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <App/>
-        </Provider>
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+const root = document.getElementById("root");
 
+ReactDOM.createRoot(root).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
