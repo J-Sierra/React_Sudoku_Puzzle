782f652035f35986afa2c58e71a497211d96b796
Johnny Sierra <Gjohnnys@att.net>
2023-02-26 19:33:35 -0600

    src/index.js updated to react 18 standards

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./comps/styles/index.scss";
 import { Provider } from "react-redux";
 import App from "./app";
-import * as serviceWorker from "./serviceWorker";
 import store from "./comps/redux/store";
 
 const root = document.getElementById("root");
@@ -13,7 +12,3 @@ ReactDOM.createRoot(root).render(
     <App />
   </Provider>
 );
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
